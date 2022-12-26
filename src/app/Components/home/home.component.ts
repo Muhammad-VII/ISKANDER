94e400ad797e291cdb81aacbc8fcc619b3b9da64
Muhammad Akmal <akmal@emojiit.com>
2022-12-26 23:24:51 +0200

    'init-integration

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { SharedService } from './../../services/shared.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _SharedService: SharedService) { 
+    this._SharedService.getHomePageData().subscribe((res: any) => {
+      console.log(res)
+    })
+  }
 
   ngOnInit(): void {
   }
--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -7,10 +7,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SharedService {
+  lang: string = localStorage.getItem('lang')! ?? 'due';
 
   constructor(private _HttpClient: HttpClient) { }
 
   sendContactForm(contactForm: {}): Observable<any> {
     return this._HttpClient.post(`${environment.apiUrl}/contact/addMessage`, contactForm)
+  }
+
+  getHomePageData(): Observable<any> {
+    return this._HttpClient.get(`${environment.apiUrl}/home/getAllSections-${this.lang}?limit=10&skip=0`)
   }
 }
